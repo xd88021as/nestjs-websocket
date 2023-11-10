ca351b335b160bcc4235a93173f13d52e4f4914a
xd88021as <37061475+xd88021as@users.noreply.github.com>
2023-11-10 14:53:41 +0800

    feat: create rate limiting middleware (#3)
    * feat: update package
    
    * feat: update README
    
    * feat: init docker compose
    
    * feat: create rate limiting middleware
    
    * feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
+import { RateLimitingMiddleware } from './rate-limiting/rate-limiting.middleware';
 
 @Module({
   imports: [DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RateLimitingMiddleware).forRoutes('GET', 'data');
+  }
+}
