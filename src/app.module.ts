9f2fd253bea3843d82319461473f3ec317890fd2
xd88021as <s560798212@gmail.com>
2023-11-12 21:31:59 +0800

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { RateLimitingMiddleware } from './rate-limiting/rate-limiting.middleware';
+import { WebsocketGateway } from './websocket/websocket.gateway';
 
 @Module({
   imports: [DataModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WebsocketGateway],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
