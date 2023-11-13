54a976959b808923cf4fdf1c2169c565b8ee4232
xd88021as <s560798212@gmail.com>
2023-11-13 16:19:14 +0800

    feat: update websocket gateway

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebsocketGateway } from './websocket.gateway';
+import { BitstampModule } from 'src/bitstamp/bitstamp.module';
 
 @Module({
+  imports: [BitstampModule],
   providers: [WebsocketGateway],
 })
 export class WebsocketModule {}
